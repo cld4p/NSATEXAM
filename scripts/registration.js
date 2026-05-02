70221a9f720e3ef41cf78289c1f2ed20a70368ee
cldp <akshaj207@gmail.com>
2026-05-03 02:52:00 +0530

    Co-authored-by: Aryan Binu <aaryanbinu07-dotcom@users.noreply.github.com> Co-authored-by: Ayush Najbile <Aayush177-cmd@users.noreply.github.com>

--- scripts/registration.js
+++ scripts/registration.js
@@ -1,15 +1,33 @@
-const name = document.querySelector("#name");
-const nameInputs = Array.from(name.querySelectorAll('input'));
+document.addEventListener('DOMContentLoaded', () => {
+    const dobInput = document.getElementById('dob-input');
 
-const nameNextBtn = document.querySelector("#name-next-btn");
+    const today = new Date();
+    const yyyy = today.getFullYear();
+    const mm = String(today.getMonth() + 1).padStart(2, '0');
+    const dd = String(today.getDate()).padStart(2, '0');
+    const formattedToday = `${yyyy}-${mm}-${dd}`;
 
+    const ageSpan = document.getElementById('age-span');
 
-const allFilledName = nameInputs.every(input => input.value.trim != "");
+    
+    dobInput.setAttribute('max', formattedToday);
 
-if(allFilledName) {
-    nameNextBtn.classList.add("active")
-} else {
-    nameNextBtn.classList.remove("active")
-}
+    dobInput.addEventListener('change', () => {
+        const birthDate = new Date(dobInput.value);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
 
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
+        }
+        
+        ageSpan.textContent = age;
 
+        if(age<16) {
+            ageSpan.style.color = 'red';
+        } else {
+            ageSpan.style.color = 'green';
+        }
+    
+    });
+});
